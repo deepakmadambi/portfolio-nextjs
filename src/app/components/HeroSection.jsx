a6d07b237ba8a704b96ba58d89479edbcea0e38d
dabacoder <trollmyfriendmaddy@gmail.com>
2023-10-19 21:42:12 +0530

    image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -71,6 +71,7 @@ const HeroSection = () => {
               src="/images/hero-image.png"
               alt="hero image"
               priority={true}
+              quality={90}
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
