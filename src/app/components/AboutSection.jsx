f94e4d8c56bdf456ead0029a9a195f330dd50905
dabacoder <trollmyfriendmaddy@gmail.com>
2023-10-19 21:21:40 +0530

    bugfixes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -58,7 +58,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I am a multi-faceted individual with a diverse set of passions and skills. As a software engineer, I'm adept at crafting digital solutions that bridge the gap between imagination and reality. Beyond the screen, I transform into a motorbike traveler, embarking on journeys that span landscapes and cultures, capturing the essence of the world. My creative side thrives as a content creator, allowing me to share my experiences and insights with the world. These pursuits fuel my desire for continuous exploration and innovation, making me a true enthusiast of life's myriad adventures.
+          I am a multi-faceted individual with a diverse set of passions and skills. As a software engineer, I am adept at crafting digital solutions that bridge the gap between imagination and reality. Beyond the screen, I transform into a motorbike traveler, embarking on journeys that span landscapes and cultures, capturing the essence of the world. My creative side thrives as a content creator, allowing me to share my experiences and insights with the world. These pursuits fuel my desire for continuous exploration and innovation, making me a true enthusiast of lifes myriad adventures.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
